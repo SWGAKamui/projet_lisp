dadaba94e8bd25e12c562aff9bade8947793d887
Rack07 <rack.kendorim@gmail.com>
2015-11-16 21:57:03 +0100

    MAJ
    bien avancé il me reste plus grand chose pour finir le solveur il reste
    toujours ce que j'ai mis précédemment

--- Classe-case.lisp
+++ Classe-case.lisp
@@ -1,33 +1,47 @@
 ;;;***************Classe case***************;;;
-;;**D√©finition de la classe
+;;**DÈfinition de la classe
 (defclass case ()
-  ((val-init :initarg :val-init :reader val-init-of)
-   (val-actu :initarg :val-actu :reader val-actu-of)
-   (possibilite :initarg :possibilite :reader possibilite-of)
+  ((val-init :initarg :val-init :accessor val-init-of)
+   (val-actu :initarg :val-actu :accessor val-actu-of)
+   (possibilite :initarg :possibilite :accessor possibilite-of)
+   (nb-pos :initarg :nb-pos :accessor nb-pos-of)
+   
    )
   )
 ;;**Affichage de la classe
 (defmethod print-object ((c case) stream)
-  (format stream "case : ~%Valeur Initiale=~d Valeur Actuelle=~d possibilit√©=~d"
+  (format stream "case : ~%Val-Init=~d Val-Actu=~d possibilitÈ=~d Nb restante=~d"
 	  (val-init-of c)
 	  (val-actu-of c)
 	  (possibilite-of c)
+	  (nb-pos-of c)
 	  )
   )
-;;**Cr√©ation d'une case
-(defun make-case (&key (val-init 0) (val-actu 0) (possibilite '(1 2 3 4 5 6 7 8 9)))
-  (make-instance 'case :val-init val-init :val-actu val-actu :possibilite possibilite)
+;;**CrÈation d'une case
+(defun make-case (&key (val-init 0) (val-actu 0) (possibilite '(1 2 3 4 5 6 7 8 9)) (nb-pos 9))
+  (make-instance 'case
+		 :val-init val-init
+		 :val-actu val-actu
+		 :possibilite possibilite
+		 :nb-pos nb-pos)
   )
-;;**Supprimer une possibilit√© d'une case 
+;;**Supprimer une possibilitÈ d'une case 
 (defun rem-possibilite (nombre case)
-  (delete nombre (possibilite-of case))
+  (when (member nombre (possibilite-of case))
+    (setf (nb-pos-of case) (1- (nb-pos-of case)))
+    (delete nombre (possibilite-of case))
+    )
   )
-;;**Cr√©ation d'une grille vide de facteur^4 ou taille^2 cases
+;;**Supprimer un nombre sur toute la ligne colonne et groupe de case.
+(defun clean-possibilite (grille nombre lig col)
+  
+  )
+;;**CrÈation d'une grille vide de facteur^4 ou taille^2 cases
 ;;Exemple de cr√©ation de grille sudoku (defparameter *t* (make-grille :facteur 3))
 (defun make-grille (&key (facteur 0) (taille 0))
   (if (zerop facteur)
       (if (zerop taille)
-	  (format t "n'importe quoi")
+	  (format t "N'importe quoi")
 	  (make-array (list taille taille) :initial-element (make-case))
 	  )
       (let ((dim (* facteur facteur)))
@@ -35,22 +49,37 @@
 	)
       )
   )
-;;Chargement d'une grille de sudoku a l'origine sous forme de tableau a 2 dimensions
+;;Chargement d'une grille de sudoku a l'origine sous forme de tableau a 2 dimensions et on en profite ‡ chaque fois pour clean les possibilitÈ
 (defun load-grille (grille)
   (let* ((taille (array-dimension grille 0))
 	 (notre-grille (make-grille :taille taille)))
-    ;cela va d√©pendre de comment sont donn√© les grilles si c'est des tableaux a deux dimensions, une liste a une dimension.... √† v√©rifier
+					;cela va dÈpendre de comment sont donnÈes les grilles si c'est des tableaux a deux dimensions, une liste a une dimension.... A vÈrifier pour chaque vlaeur diffÈrente de 0 on les enlËve des possibilitÈ
     )
   )
 
 ;;Solveur
 ;;Solveur assez simple au final
-;;1 on parcours toute les cases
-;;     lorsque valeur initiale diff√©rente de 0 on enl√®ve sa possibilit√© des cases concern√©
 ;;2ieme parcours tant qu'il y a des valeur actuelles=0
 ;;on parcours toute les cases
-;;         lorsque (length possibilit√©=1) alors
+;;         lorsque (nb-pos=1) alors
 ;;                  valeur actuelle=first possibilit√©
 ;;                  rem possibilit√©
 (defun solver (grille)
+  (let* ((taille (array-dimension grille 0))
+	 (c (aref grille 0 0))
+	 (fini 0))
+    (loop while (zerop fini) do
+      (setq fini 1)
+      (dotimes (i taille t)
+	(dotimes (j taille t)
+	  (setq c (aref grille i j))
+	  (when (and (zerop (val-init-of c)) (= 1 (nb-pos-of c)));il n'y a qu'une possibilitÈ ?
+	    (setf (val-actu-of c) (first (possibilite-of c)))
+	    (setq fini 0)
+	    (clean-possibilite grille (val-actu-of c) i j)
+	    )
+	  )
+	)
+      )
+    )
   )
